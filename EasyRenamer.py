6a59407841267848bf1fd262c11bf07db21eb8e5
Torreip <80983183+Torreip@users.noreply.github.com>
2022-02-25 15:01:14 +0100

    See the latest release to see complete changelog

--- EasyRenamer.py
+++ EasyRenamer.py
@@ -2,7 +2,20 @@ import os
 import sys
 import time
 
-#MassRenameFunction
+#ContinueFuntion
+def choice():
+    print("Do you want to rename other files ? ")
+    c1 = input("Make your choice (Y or n) ")
+    c1 = c1.lower()
+
+    if c1 == "y" or c1 == "ye" or c1 == "yes":
+        main()
+    else:
+        print("\33[94mThank's for using my program (V1.2) ! \nSigned Torreip 2022")
+        time.sleep(2)
+        sys.exit()
+
+#EasyRenamerMainFunction
 def main():
     folder = ""
     while folder == "":
@@ -10,7 +23,7 @@ def main():
         name = str(input("What should it be renamed to ? "))
     #MainLoop
     for count, filename in enumerate(os.listdir(folder)):
-        if count < 10:
+        if count < 9:
             number = f"0{str(count+1)}"
         else:
             number = f"{str(count+1)}"
@@ -19,20 +32,18 @@ def main():
         dst = f"{str(name)}{str(number)}{str(fext)}"
         src =f"{folder}/{filename}"
         dst =f"{folder}/{dst}"
-         
-        os.rename(src, dst)
-    print(f"Every file in '{str(folder)}' has been renamed.")
-    print("Do you want to rename other files ? ")
-    c1 = input("Make your choice (Y or n) ")
-    c1 = c1.lower()
-
-    if c1 == "y" or c1 == "ye" or c1 == "yes":
-        main()
+        ex = 0
+        try:
+            os.rename(src, dst)
+        except FileExistsError:
+            ex = ex + 1
+            pass
+    if ex == 0:
+        print(f"Every file in '{str(folder)}' has been renamed.")
     else:
-        print("\33[94mThank's for using my program ! \nSigned Torreip 2022")
-        time.sleep(2)
-        sys.exit()
-
+        print(f"Every file in '{str(folder)}' has been renamed.")
+        print(f"{str(count+2 - ex)} files already beared their new name.")
+    choice()
  
 if __name__ == '__main__':
     main()
